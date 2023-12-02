85eb52b3c6ed8f8150a955c1e35bc7bbb9353735
Elmerfjot <elmerfjots@gmail.com>
2023-12-02 16:27:02 +0100

    Preparation

---
+++ AdventOfCodeFoundation/Solvers/2023/Day10Solver2023.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCodeFoundation.IO;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+
+namespace AdventOfCodeFoundation.Solvers._2023
+{
+    [Solves("2023/12/10")]
+    internal class Day10Solver2023 : ISolver
+    {
+        public async Task<string> SolvePartOne(Input input)
+        {
+            var raw = await input.GetRawInput();
+           
+            return "";
+        }
+
+        public async Task<string> SolvePartTwo(Input input)
+        {
+            var raw = await input.GetRawInput();
+            
+            return "";
+        }
+    }
+}
