2a0c52a1baf19b65ac50d8ba58789f8b21fb7687
KARA <95501069+elmerfjots@users.noreply.github.com>
2023-12-04 07:17:00 +0100

    Day4 part1

--- AdventOfCodeFoundation/Solvers/2023/Day4Solver2023.cs
+++ AdventOfCodeFoundation/Solvers/2023/Day4Solver2023.cs
@@ -11,15 +11,42 @@ namespace AdventOfCodeFoundation.Solvers._2023
         public async Task<string> SolvePartOne(Input input)
         {
             var raw = await input.GetRawInput();
-           
-            return "";
+            var rows = raw.Split("\r\n").Select(x => new ScratchCard(x));
+
+            var intersects = rows.Select(x => x.Numbers.Intersect(x.WinningNumbers)).ToList();
+            var sums = new List<int>();
+            foreach (var row in intersects.Where(x => x.Any()).Select(x => x.ToList()))
+            {
+                if (row.Count == 1) { sums.Add(1); continue; }
+                var d = 1;
+                for (var i = 1; i < row.Count; i++)
+                {
+                    d = d * 2;
+                }
+                sums.Add((int)d);
+            }
+            return sums.Sum().ToString();
         }
 
         public async Task<string> SolvePartTwo(Input input)
         {
             var raw = await input.GetRawInput();
-            
+
             return "";
+        }
+
+        private class ScratchCard
+        {
+            public List<int> WinningNumbers { get; set; }
+            public List<int> Numbers { get; set; }
+
+            public ScratchCard(string x)
+            {
+                var s = x.Split(": ").Last().Split(" | ");
+                WinningNumbers = s.First().Split(" ").Where(x => string.IsNullOrEmpty(x) == false).Select(n => int.Parse(n)).ToList();
+                Numbers = s.Last().Split(" ").Where(x => string.IsNullOrEmpty(x) == false).Select(n => int.Parse(n.Trim())).ToList();
+
+            }
         }
     }
 }
