8c59dc1822fee293b9771933e69fe9a3cf52d70e
Elmerfjot <elmerfjots@gmail.com>
2023-12-05 14:57:11 +0100

    Day 4  done but needs optimization.. Day 5 part one done. Part 2 needs optimization in order to work

--- AdventOfCodeFoundation/Solvers/2023/Day4Solver2023.cs
+++ AdventOfCodeFoundation/Solvers/2023/Day4Solver2023.cs
@@ -11,41 +11,76 @@ namespace AdventOfCodeFoundation.Solvers._2023
         public async Task<string> SolvePartOne(Input input)
         {
             var raw = await input.GetRawInput();
-            var rows = raw.Split("\r\n").Select(x => new ScratchCard(x));
-
-            var intersects = rows.Select(x => x.Numbers.Intersect(x.WinningNumbers)).ToList();
-            var sums = new List<int>();
-            foreach (var row in intersects.Where(x => x.Any()).Select(x => x.ToList()))
-            {
-                if (row.Count == 1) { sums.Add(1); continue; }
-                var d = 1;
-                for (var i = 1; i < row.Count; i++)
-                {
-                    d = d * 2;
-                }
-                sums.Add((int)d);
-            }
-            return sums.Sum().ToString();
+            var cards = raw.Split("\r\n").Select(x => new ScratchCard(x));
+            return cards.Sum(x => x.Points).ToString();
         }
 
         public async Task<string> SolvePartTwo(Input input)
         {
             var raw = await input.GetRawInput();
+            var cards = raw.Split("\r\n").Select(x => new ScratchCard(x));
+            var cardDict = cards.ToDictionary(x => x.Id);
 
-            return "";
+            var wonCards = new List<ScratchCard>();
+            var cardsToProcess = new Queue<ScratchCard>();
+            foreach (var c in cards)
+            {
+                cardsToProcess.Enqueue(c);
+                wonCards.Add(c);
+            }
+            while (cardsToProcess.Any(x => x.IsProcessed == false))
+            {
+                var card = cardsToProcess.Dequeue();
+                for (int i = 1; i <= card.MatchingNumbers.Count; i++)
+                {
+                    var cardNumber = card.CardNumber;
+                    if (cardDict.ContainsKey($"{cardNumber + i}") == false)
+                    {
+
+                        throw new Exception($"Key {cardNumber + i} was not present...");
+                    }
+                    cardsToProcess.Enqueue(cardDict[$"{cardNumber + i}"]);
+                    wonCards.Add(card);
+                }
+            }
+            return wonCards.Count.ToString();
         }
-
         private class ScratchCard
         {
             public List<int> WinningNumbers { get; set; }
             public List<int> Numbers { get; set; }
-
+            public bool IsProcessed { get; set; } = false;
+            public List<int> MatchingNumbers { get; set; }
+            public string Id { get; set; }
+            public int CardNumber { get; set; }
+            public int Points { get; set; }
+            public List<ScratchCard> Copies { get; set; }
             public ScratchCard(string x)
             {
-                var s = x.Split(": ").Last().Split(" | ");
+                var fSplit = x.Split(": ");
+                Id = fSplit[0].Trim().Replace("Card", "").Trim();
+                CardNumber = int.Parse(Id.Split(" ").Last().Trim());
+                var s = fSplit.Last().Split(" | ");
                 WinningNumbers = s.First().Split(" ").Where(x => string.IsNullOrEmpty(x) == false).Select(n => int.Parse(n)).ToList();
                 Numbers = s.Last().Split(" ").Where(x => string.IsNullOrEmpty(x) == false).Select(n => int.Parse(n.Trim())).ToList();
 
+                MatchingNumbers = Numbers.Intersect(WinningNumbers).ToList();
+
+                foreach (var row in MatchingNumbers)
+                {
+                    if (MatchingNumbers.Count == 1) { Points = 1; continue; }
+                    var d = 1;
+                    for (var i = 1; i < MatchingNumbers.Count; i++)
+                    {
+                        d = d * 2;
+                    }
+                    Points = d;
+                }
+
+            }
+            public override string ToString()
+            {
+                return Id.ToString();
             }
         }
     }
--- AdventOfCodeFoundation/Solvers/2023/Day5Solver2023.cs
+++ AdventOfCodeFoundation/Solvers/2023/Day5Solver2023.cs
@@ -1,7 +1,4 @@
 ﻿using AdventOfCodeFoundation.IO;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Threading;
 
 namespace AdventOfCodeFoundation.Solvers._2023
 {
@@ -11,15 +8,129 @@ namespace AdventOfCodeFoundation.Solvers._2023
         public async Task<string> SolvePartOne(Input input)
         {
             var raw = await input.GetRawInput();
-           
-            return "";
+            var rows = raw.Split("\r\n").Where(x => !x.Equals("")).ToList();
+            var seeds = rows.First().Split(": ").Last().Trim().Split(" ").Select(x => long.Parse(x)).ToList();
+            var minLocation = GetMinLocation(seeds, ParseMaps(rows));
+            return minLocation.ToString();
         }
-
         public async Task<string> SolvePartTwo(Input input)
         {
             var raw = await input.GetRawInput();
-            
-            return "";
+            var rows = raw.Split("\r\n").Where(x => !x.Equals("")).ToList();
+            var seedRaw = rows.First().Split(": ").Last().Trim().Split(" ").Select(long.Parse).ToList();
+            var ranges = new Dictionary<long, List<long>>();
+            var maps = ParseMaps(rows);
+            for (var i = 0; i < seedRaw.Count; i += 2)
+            {
+                var start = seedRaw[i];
+                var range = seedRaw[i + 1];
+                ranges.Add(start, new List<long>());
+                //seeds.Add(start);
+                for (var k = start; k < start + range; k++)
+                {
+                    ranges[start].Add(k);
+                }
+            }
+
+
+            foreach(var m in maps)
+            {
+                var ordered = m.Values.OrderBy(x => x.SourceMinMax);
+                var newRanges = new L
+            }
+
+
+
+
+            (long initial, long minValue) minLocation = (-1, long.MaxValue);
+            foreach (var s in seeds)
+            {
+                var l = GetMinLocation(s.Value, maps); ;
+                minLocation = (minLocation.minValue > l) ? (s.Key, l) : minLocation;
+            }
+
+            return minLocation.ToString();
+        }
+        private List<Map> ParseMaps(List<string> rows)
+        {
+            var stsIdx = rows.IndexOf("seed-to-soil map:");
+            var stfIdx = rows.IndexOf("soil-to-fertilizer map:");
+            var ftwIdx = rows.IndexOf("fertilizer-to-water map:");
+            var wtlIdx = rows.IndexOf("water-to-light map:");
+            var lttIdx = rows.IndexOf("light-to-temperature map:");
+            var tthIdex = rows.IndexOf("temperature-to-humidity map:");
+            var htlIdx = rows.IndexOf("humidity-to-location map:");
+            var seedToSoilMap = new Map(rows.Skip(1 + stsIdx).Take((stfIdx - 1) - stsIdx).ToList());
+            var soilToFertilizerMap = new Map(rows.Skip(1 + stfIdx).Take((ftwIdx - 1) - stfIdx).ToList());
+            var fertilizerToWaterMap = new Map(rows.Skip(1 + ftwIdx).Take((wtlIdx - 1) - ftwIdx).ToList());
+            var waterToLightMap = new Map(rows.Skip(1 + wtlIdx).Take((lttIdx - 1) - wtlIdx).ToList());
+            var lightToTemperatureMap = new Map(rows.Skip(1 + lttIdx).Take((tthIdex - 1) - lttIdx).ToList());
+            var temperatureToHumidityMap = new Map(rows.Skip(1 + tthIdex).Take((htlIdx - 1) - tthIdex).ToList());
+            var himidityToLocationMap = new Map(rows.Skip(1 + htlIdx).Take((rows.Count - 1) - htlIdx).ToList());
+
+            return new List<Map> { seedToSoilMap, soilToFertilizerMap, fertilizerToWaterMap, waterToLightMap, lightToTemperatureMap, temperatureToHumidityMap, himidityToLocationMap };
+        }
+        private long GetMinLocation(List<long> seeds, List<Map> maps)
+        {
+            var minLocation = long.MaxValue;
+            foreach (var s in seeds)
+            {
+                var l1 = maps[0].GetValueForLong(s);
+                var l2 = maps[1].GetValueForLong(l1);
+                var l3 = maps[2].GetValueForLong(l2);
+                var l4 = maps[3].GetValueForLong(l3);
+                var l5 = maps[4].GetValueForLong(l4);
+                var l6 = maps[5].GetValueForLong(l5);
+                var l7 = maps[6].GetValueForLong(l6);
+                minLocation = (l7 < minLocation) ? l7 : minLocation;
+            }
+            return minLocation;
+        }
+        private class Map
+        {
+            public List<MapNode> Values { get; set; }
+            public Map(List<string> stringMap)
+            {
+                Values = new List<MapNode>();
+                foreach (var s in stringMap)
+                {
+                    var destinationRangeStart = long.Parse(s.Split(" ").First());
+                    var sourceRangeStart = long.Parse(s.Split(" ")[1]);
+
+                    var rangeLength = long.Parse(s.Split(" ").Last());
+
+                    var destinationRangeMax = destinationRangeStart + rangeLength - 1;
+                    var sourceRangeMax = sourceRangeStart + rangeLength - 1;
+
+                    Values.Add(new MapNode((sourceRangeStart, sourceRangeMax), (destinationRangeStart, destinationRangeMax)));
+                }
+            }
+            public long GetValueForLong(long l)
+            {
+                foreach (var v in Values)
+                {
+                    if (l >= v.SourceMinMax.min && l <= v.SourceMinMax.max)
+                    {
+                        var dest = Math.Abs(l - v.SourceMinMax.min);
+
+                        var d = v.DestinationMinMax.min + dest;
+
+                        return d;
+                    }
+                }
+
+                return l;
+            }
+        }
+        class MapNode
+        {
+            public (long min, long max) DestinationMinMax { get; set; }
+            public (long min, long max) SourceMinMax { get; set; }
+            public MapNode((long min, long max) sourceMinMax, (long min, long max) destMinMax)
+            {
+                DestinationMinMax = destMinMax;
+                SourceMinMax = sourceMinMax;
+            }
         }
     }
 }
