a3af81e0b9081d253601508b1950ff67d74f86f4
KARA <95501069+elmerfjots@users.noreply.github.com>
2023-12-09 09:51:45 +0100

    old stuff

---
+++ AdventOfCodeFoundation/Solvers/2015/Day1Solver2015.cs
@@ -0,0 +1,37 @@
+﻿using AdventOfCodeFoundation.IO;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+
+namespace AdventOfCodeFoundation.Solvers._2023
+{
+    [Solves("2015/12/1")]
+    internal class Day1Solver2015 : ISolver
+    {
+        Dictionary<char, int> instructions = new Dictionary<char, int>() { { '(', 1 }, { ')', -1 } };
+        public async Task<string> SolvePartOne(Input input)
+        {
+            var rawInput = await input.GetRawInput();
+            var s = 0;
+            foreach(var i in rawInput)
+            {
+                s += instructions[i];
+            }
+            return s.ToString();
+        }
+
+        public async Task<string> SolvePartTwo(Input input)
+        {
+            var rawInput = await input.GetRawInput();
+            var s = 0;
+            var c = 0;
+            foreach (var i in rawInput)
+            {
+                s += instructions[i];
+                c++;
+                if (s == -1) { break; }
+            }
+            return c.ToString();
+        }
+    }
+}
---
+++ AdventOfCodeFoundation/Solvers/2015/Day2Solver2015.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCodeFoundation.IO;
+namespace AdventOfCodeFoundation.Solvers._2023
+{
+    [Solves("2015/12/2")]
+    internal class Day2Solver2015 : ISolver
+    {
+        public async Task<string> SolvePartOne(Input input)
+        {
+            var raw = await input.GetRawInput();
+            var rows = raw.Split("\r\n").Select(x => x.Split("x").Select(x => int.Parse(x)).ToList());
+            return rows.Sum(GetWrappingPaperAmount).ToString();
+        }
+        
+        public async Task<string> SolvePartTwo(Input input)
+        {
+            var raw = await input.GetRawInput();
+            var rows = raw.Split("\r\n").Select(x => x.Split("x").Select(x => int.Parse(x)).ToList());
+            return rows.Sum(GetRibbonAmount).ToString();
+        }
+        private int GetRibbonAmount(List<int> dimensions)
+        {
+            (int l, int w, int h) = (dimensions[0], dimensions[1], dimensions[2]);
+            var orderedSides = (new int[] { l, w, h }).Order().ToList();
+            var r1 = orderedSides[0] + orderedSides[0] + orderedSides[1] + orderedSides[1];
+            var r2 = orderedSides[0] * orderedSides[1] * orderedSides[2];
+            return r1+r2;
+        }
+        private int GetWrappingPaperAmount(List<int> dimensions)
+        {
+            (int l, int w, int h) = (dimensions[0], dimensions[1], dimensions[2]);
+            (int s1, int s2, int s3) = ((l * w), (w * h), (h * l));
+            var smallestSide = (new int[] { s1, s2, s3 }).Min();
+            return (2 * s1) + (2 * s2) + (2 * s3) + smallestSide;
+        }
+    }
+}
