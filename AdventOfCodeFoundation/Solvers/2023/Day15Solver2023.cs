530bf78a1c5c6e45efb2ffc1ae70c4e9f6f1379f
Elmerfjot <elmerfjots@gmail.com>
2023-12-16 12:59:24 +0100

    Day15part1

--- AdventOfCodeFoundation/Solvers/2023/Day15Solver2023.cs
+++ AdventOfCodeFoundation/Solvers/2023/Day15Solver2023.cs
@@ -11,15 +11,33 @@ namespace AdventOfCodeFoundation.Solvers._2023
         public async Task<string> SolvePartOne(Input input)
         {
             var raw = await input.GetRawInput();
-           
-            return "";
+            var strings = raw.Split(",");
+            var sum = strings.Sum(GetHashOfString);
+            return sum.ToString();
         }
 
         public async Task<string> SolvePartTwo(Input input)
         {
             var raw = await input.GetRawInput();
-            
+
             return "";
+        }
+        private long GetHashOfString(string s)
+        {
+            /*  Determine the ASCII code for the current character of the string.
+                    Increase the current value by the ASCII code you just determined.
+                    Set the current value to itself multiplied by 17.
+                    Set the current value to the remainder of dividing itself by 256.
+                */
+            long currentValue = 0;
+            foreach (var c in s)
+            {
+                int asciiCode = (int)c;
+                currentValue += asciiCode;
+                currentValue *= 17;
+                currentValue %= 256;
+            }
+            return currentValue;
         }
     }
 }
