cab66f12c1ca20d303230bcea15ad19239e75793
KARA <95501069+elmerfjots@users.noreply.github.com>
2023-12-17 00:57:50 +0100

    Prep

---
+++ AdventOfCodeFoundation/Solvers/2023/Day21Solver2023.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCodeFoundation.IO;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+
+namespace AdventOfCodeFoundation.Solvers._2023
+{
+    [Solves("2023/12/21")]
+    internal class Day21Solver2023 : ISolver
+    {
+        public async Task<string> SolvePartOne(Input input)
+        {
+            var raw = await input.GetRawInput();
+           
+            return "";
+        }
+
+        public async Task<string> SolvePartTwo(Input input)
+        {
+            var raw = await input.GetRawInput();
+            
+            return "";
+        }
+    }
+}
