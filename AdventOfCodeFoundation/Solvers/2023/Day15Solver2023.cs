a0b087b35f3dfc4f0ee1c99bb699aca2e3e27571
KARA <95501069+elmerfjots@users.noreply.github.com>
2023-12-16 15:16:37 +0100

    Day15 part2

--- AdventOfCodeFoundation/Solvers/2023/Day15Solver2023.cs
+++ AdventOfCodeFoundation/Solvers/2023/Day15Solver2023.cs
@@ -1,7 +1,4 @@
 ﻿using AdventOfCodeFoundation.IO;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Threading;
 
 namespace AdventOfCodeFoundation.Solvers._2023
 {
@@ -19,9 +16,30 @@ namespace AdventOfCodeFoundation.Solvers._2023
         public async Task<string> SolvePartTwo(Input input)
         {
             var raw = await input.GetRawInput();
-
-            return "";
+            var lenses = raw.Split(",").Select(x => new Lens(x));
+            var boxes = Enumerable.Range(0, 256).Select(x => new Dictionary<string, long>()).ToList();
+            foreach (var lens in lenses) { CalculateBoxesForLens(ref boxes, lens); }
+            return CalculateSumOfFocusingPower(ref boxes).ToString();
         }
+
+        private long CalculateSumOfFocusingPower(ref List<Dictionary<string, long>> boxes)
+        {
+            var sum = 0L;
+            for (var boxNumber = 0; boxNumber < boxes.Count; boxNumber++)
+            {
+                var box = boxes[boxNumber];
+
+                for (var slot = 0; slot < box.Count; slot++)
+                {
+                    var e = box.ElementAt(slot);
+                    var focusingPower = (boxNumber + 1) * (slot + 1) * e.Value;
+                    sum += focusingPower;
+                }
+
+            }
+            return sum;
+        }
+
         private long GetHashOfString(string s)
         {
             /*  Determine the ASCII code for the current character of the string.
@@ -38,6 +56,52 @@ namespace AdventOfCodeFoundation.Solvers._2023
                 currentValue %= 256;
             }
             return currentValue;
+        }
+        private void CalculateBoxesForLens(ref List<Dictionary<string, long>> boxes, Lens lens)
+        {
+            var relevantBoxNumber = (int)GetHashOfString(lens.Label);
+            if (lens.Operation.Equals("="))
+            {
+                if (boxes[relevantBoxNumber].ContainsKey(lens.Label))
+                {
+                    boxes[relevantBoxNumber][lens.Label] = lens.FocalLength;
+                }
+                else
+                {
+                    boxes[relevantBoxNumber] = boxes[relevantBoxNumber]
+                        .Append(new KeyValuePair<string, long>(lens.Label, lens.FocalLength))
+                        .ToDictionary(x => x.Key, x => x.Value);
+                }
+            }
+            if (lens.Operation.Equals("-"))
+            {
+                if (boxes[relevantBoxNumber].ContainsKey(lens.Label))
+                {
+                    boxes[relevantBoxNumber].Remove(lens.Label);
+                }
+            }
+        }
+        class Lens
+        {
+            public string Label { get; set; }
+            public long FocalLength { get; set; }
+            public string Operation { get; set; }
+            public Lens(string input)
+            {
+                string[] ss = new string[2];
+                if (input.Contains('='))
+                {
+                    ss = input.Split('=');
+                    FocalLength = long.Parse(ss[1]);
+                    Operation = "=";
+                }
+                else if (input.Contains('-'))
+                {
+                    ss = input.Split('-');
+                    Operation = "-";
+                }
+                Label = ss.First();
+            }
         }
     }
 }
